b286d6987a4f42bd6b2c51366f24d3444690d605
victorsworld <victordirect48@gmail.com>
2023-02-22 11:34:49 -0500

    finished

--- main.js
+++ main.js
@@ -4,11 +4,69 @@
 
 
 
+function createUser(firstName, lastname){
+    let user = {
+        firstName: firstName,
+        lastName: lastname
+    }
+    return user;
+}
 
+function setAge(user, num){
+    user.age = num;
+    return user;
+}
 
+function incrementAge(user){
+   user.age = user.age + 1;
 
+    return user;
+}
 
+function fixCar(car){
+    car.needsMaintenance = false;
 
+    return car
+}
+
+function addGrades(student, newGrades){
+    student.grades = student.grades.concat(newGrades) 
+
+    return student
+}
+
+function getDataType(value,key){
+   let a = value[key]
+
+    return typeof a;
+}
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo);
+
+    return todos;
+}
+
+function addSong(playlist, newSong){
+    playlist.duration = playlist.duration + newSong.duration;
+    playlist.songs.push(newSong);
+ return playlist
+}
+
+function updateReportCard(reportCard, num){
+if(num < reportCard.lowestGrade){
+    reportCard.lowestGrade = num
+} else if( num > reportCard.highestGrade){
+    reportCard.highestGrade = num
+} 
+reportCard.grades.push(num);
+let sum = 0;
+for(let i = 0; i < reportCard.grades.length; i++){
+    sum += reportCard.grades[i]
+}
+reportCard.averageGrade = sum/4;
+return reportCard;
+}
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
